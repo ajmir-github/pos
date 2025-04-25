d826ae04382b3d24c075373523384e6cc60d6e3b
Ajmir Raziqi <ajmir.ng3@gmail.com>
2025-04-25 16:50:16 +0300

    same

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-export type * from "@prisma/client";
+import { PrismaClient } from "../database";
+export type * from "../database";
 
 export const database = new PrismaClient();
