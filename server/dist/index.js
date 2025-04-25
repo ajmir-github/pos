331e2ae678f231def26cfd0b693c0183926f0281
Ajmir Raziqi <ajmir.ng3@gmail.com>
2025-04-25 16:21:18 +0300

    agian

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -10,13 +19,21 @@ const cors_1 = __importDefault(require("cors"));
 const constants_1 = require("./constants");
 const controllers_1 = require("./controllers");
 const getLocalIP_1 = __importDefault(require("./utils/getLocalIP"));
+const database_1 = require("./database");
+// express
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)(constants_1.corsOptions));
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const colors = yield database_1.database.color.findMany();
+    res.json(colors);
+}));
+// socket
 const httpServer = (0, http_1.createServer)(app);
 const io = new socket_io_1.Server(httpServer, {
     cors: constants_1.corsOptions,
 });
 io.on("connection", controllers_1.socketHandler);
+// listen
 // const host = "192.168.14.209";
 if (constants_1.DevMode) {
     const host = (0, getLocalIP_1.default)(); // localhost and local IP Address
--- server/dist/test.js
+++ server/dist/test.js
@@ -1,9 +1,27 @@
 "use strict";
-function sum(a, b) {
-    return a + b;
-}
-function sumChain(a) {
-    return function (b) {
-        return sum(a, b);
-    };
-}
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
+database_1.database.color
+    .create({
+    data: {
+        name: "Red",
+        class: "bg-red-500",
+    },
+})
+    .then((res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(res);
+    const colors = yield database_1.database.color.findMany();
+    console.log(colors);
+}))
+    .catch((err) => {
+    console.log(err);
+});
